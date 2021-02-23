caaa54efd4e00734677945beee39e2a324a75667
Omair Muhi <omairmuhi@icloud.com>
2021-02-23 08:22:27 -0500

    Add comment

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// use production or development DB
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/budget", {
         useNewUrlParser: true,
