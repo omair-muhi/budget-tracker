b55a8064f64f9f5ffc323b03d4a7de5c3452ba2c
Omair Muhi <omairmuhi@icloud.com>
2021-02-22 23:22:05 -0500

    Subject: Intercept network requests Problem: NA Solution: NA Note: NA

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,9 +12,9 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 // install
 self.addEventListener("install", function(evt) {
     // pre cache transaction data
-    // evt.waitUntil(
-    //     caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    // );
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
 
     // pre cache all static assets
     evt.waitUntil(
@@ -46,6 +46,27 @@ self.addEventListener("activate", function(evt) {
 
 // intercept network requests
 self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
     // serve static assets
     evt.respondWith(
         caches.open(CACHE_NAME).then(cache => {
